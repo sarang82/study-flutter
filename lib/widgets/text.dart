0621387f1028b12fcf56a6896170331a6fc99312
sarang <julove0802@gmail.com>
2025-03-16 22:58:42 +0900

    프로젝트 구조 수정

---
+++ lib/widgets/text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        body:Center(
+          child:Text(
+            '플러터 공부',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.orangeAccent,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
